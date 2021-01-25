6294c97eaa451d32bc8d5222683d9699f3c0363b
ahmedelmoughazy <ahmed.refaat@ibtikar.net.sa>
2021-01-25 21:54:03 +0200

    Initial Commit

---
+++ SwiftUIExample/Scene/ContentView.swift
@@ -0,0 +1,96 @@
+//
+//  ContentView.swift
+//  SwiftUIExample
+//
+//  Created by Ahmed Refaat on 17/01/2021.
+//
+
+import SwiftUI
+
+struct HeaderGradient: View {
+    var body: some View {
+        LinearGradient(gradient: Gradient(colors: [Color(Asset.Colors.pacificBlue.name),
+                                                   Color(Asset.Colors.seaBlue.name)]),
+                       startPoint: .topLeading,
+                       endPoint: .bottomTrailing)
+            .cornerRadius(20, corners: [.bottomRight])
+            .ignoresSafeArea()
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        ZStack(alignment: .top) {
+            Color(Asset.Colors.ghostWhite.name).ignoresSafeArea()
+            HeaderView()
+            ZStack {
+                GeometryReader { geo in
+                    Color.white.frame(width: geo.size.width, height: geo.size.height)
+                        .cornerRadius(16)
+                        .shadow(color: Color(Asset.Colors.taupeGray.color.withAlphaComponent(0.4)), radius: 16)
+                }
+                VStack(spacing: 30) {
+                    DetailsView().offset(x: 0, y: -20).padding(.horizontal, 12)
+                    Spacer()
+                }
+            }
+            .padding(.top, 110)
+            .frame(width: UIScreen.main.bounds.width - 30)
+        }
+    }
+}
+
+struct InfoTextView: View {
+    var title: String
+    var subtitle: String
+        
+    var body: some View {
+        HStack(spacing: 28) {
+            Spacer()
+            Text(subtitle)
+                .font(.custom(FontFamily._29LTAzer.medium.name, size: 17))
+                .foregroundColor(Color(Asset.Colors.steelBlue.name))
+            Text(title)
+                .frame(width: 95, alignment: .trailing)
+                .font(.custom(FontFamily._29LTAzer.regular.name, size: 17))
+                .foregroundColor(Color(Asset.Colors.darkSlateGray.name))
+        }
+    }
+}
+
+struct DetailsView: View {
+    var body: some View {
+        Group {
+            Image(Asset.Assets.icAvatarBg.name)
+            
+            InfoTextView(title: "صاحب المنشأة", subtitle: "محمد أحمد")
+            InfoTextView(title: "نوع المنشأة", subtitle: "فكرة")
+            InfoTextView(title: "القطاع", subtitle: "التقنية")
+            InfoTextView(title: "تخصص المنشأة", subtitle: "تطبيقات الجوال")
+            InfoTextView(title: "تاريخ التأسيس", subtitle: "٢٠١٩")
+            
+            Color(Asset.Colors.taupeGray.color.withAlphaComponent(0.4))
+                .frame(height: 1)
+                .padding(.horizontal, 2)
+            
+            HStack {
+                Spacer()
+                Text(L10n.aboutUs)
+                    .font(.custom(FontFamily._29LTAzer.medium.name, size: 22))
+                    .foregroundColor(Color(Asset.Colors.steelBlue.name))
+            }
+            
+            Text(L10n.about)
+                .font(.custom(FontFamily._29LTAzer.regular.name, size: 15))
+                .lineSpacing(18)
+                .foregroundColor(Color(Asset.Colors.auroMetalSaurus.name))
+                .multilineTextAlignment(.trailing)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIExample/Scene/HeaderView.swift
@@ -0,0 +1,53 @@
+//
+//  HeaderView.swift
+//  SwiftUIExample
+//
+//  Created by Ahmed Refaat on 25/01/2021.
+//
+
+import SwiftUI
+
+struct HeaderView: View {
+    var body: some View {
+        VStack {
+            ZStack(alignment: .top) {
+                HeaderGradient().frame(height: 220)
+                VStack {
+                    HStack {
+                        Button(action: {
+                            print("Edit Button Action")
+                        }, label: {
+                            Image(Asset.Assets.icEdit.name)
+                        })
+                        Spacer()
+                        Button(action: {
+                            print("Back Button Action")
+                        }, label: {
+                            Image(Asset.Assets.icBackArrow.name)
+                        })
+                    }
+                    .padding([.leading, .trailing], 18)
+                    .padding(.top, 10)
+                    
+                    Spacer().frame(height: 18)
+                    HStack {
+                        Spacer()
+                        Text("معلوماتك")
+                            .font(.custom(FontFamily._29LTAzer.bold.name, size: 22))
+                            .foregroundColor(.white)
+                        Color(Asset.Colors.nonPhotoBlue.name)
+                            .frame(width: 4, height: 23)
+                            .cornerRadius(2)
+                    }.padding(.trailing, 18)
+                }
+            }
+        }
+    }
+}
+
+
+struct HeaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        HeaderView()
+    }
+}
